0c9c73ce0cabd98aca90e5bf57ff81334cdf92b5
Angel-Dijoux <angel.dijoux@yahoo.com>
2024-02-29 23:12:12 +0100

    feat: write gql client

--- src/llq_website/partner/get_partners.py
+++ src/llq_website/partner/get_partners.py
@@ -1,9 +1,5 @@
-import requests
-import json
+from src.llq_website.client.graphql_client import GraphQLClient
 
-from src.llq_website.utils import base_url
-
-# Define your GraphQL query
 query = """
 query partners (
   $first:Int, 
@@ -35,22 +31,11 @@ query partners (
 }
 """
 
-# Define your variables
 variables = {
-    "first": 60,  # Number of items to retrieve
+    "first": 60,
 }
-
-# Define your endpoint URL
-url = f"{base_url}/graphql"
-
-# Make the request
-response = requests.post(url, json={"query": query, "variables": variables})
 
 
 def get_partners():
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["partners"]["nodes"]
-    else:
-        print("Error:", response.status_code)
-        print(response.text)
+    data = GraphQLClient(varaibles=variables, query=query).get()
+    return data["data"]["partners"]["nodes"]
