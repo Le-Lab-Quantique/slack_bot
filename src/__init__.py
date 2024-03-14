aceb0f0f92f26d3ae24697bbfacadba3394c317c
Angel-Dijoux <angel.dijoux@yahoo.com>
2024-03-14 23:18:46 +0100

    fix: tests

--- src/__init__.py
+++ src/__init__.py
@@ -10,24 +10,24 @@ from src.slack import register_listeners
 
 
 env = os.environ.get("ENV")
-config = load_config(env)
 
 app: Flask = Flask(__name__, instance_relative_config=True)
-slack_app = App(
-    token=config.SLACK_BOT_TOKEN,
-    signing_secret=config.SLACK_SIGN_IN_SECRET,
-)
+
+
+def create_slack_app(environment=None):
+    config = load_config(environment or env)
+    return App(token=config.SLACK_BOT_TOKEN, signing_secret=config.SLACK_SIGN_IN_SECRET)
 
 
 def create_app(environment=None) -> Flask:
-    env = environment or os.environ.get("ENV")
-    config = load_config(env)
+    config = load_config(environment or env)
     app.config.from_object(config)
     return app
 
 
 def register_slack_handlers():
     logging.basicConfig(level=logging.DEBUG)
+    slack_app = create_slack_app()
     register_listeners(slack_app)
     handler = SlackRequestHandler(slack_app)
 
